0dc3305a17db609b522abb940e69e1fad52e9cdf
zhangzizhao404 <kongqi404@outlook.com>
2020-02-16 20:31:00 +0800

    存储

---
+++ leetcode/周赛/176.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+int countNegatives(vector<vector<int>> &grid) {
+    vector<vector<int>> as;
+    return 0;
+}
+int main() {
+    vector<vector<int>> text;
+    countNegatives(text);
+    return 0;
+}
